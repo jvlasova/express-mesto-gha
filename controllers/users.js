65c9263e3d901c1a50d6c53b1de32dc224ca4019
Власова <jvlasova@yandex.ru>
2022-09-02 09:14:03 +0300

    Project_13.2

--- controllers/users.js
+++ controllers/users.js
@@ -51,7 +51,7 @@ const createUser = (req, res) => {
 
 const updateUserInfo = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true , runValidators: true})
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true , runValidators: true}).orFail()
     .then((user) => {
       if (user) {
         res.send(user);
@@ -76,7 +76,7 @@ const updateUserInfo = (req, res) => {
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then(user => {
       if (user) {
         res.send(user);
